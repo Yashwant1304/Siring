82e563afa5ae232640f3b66aa3f597be8a812874
Yashwant1304 <yksinghemail@gmail.com>
2025-04-18 16:42:25 +0530

    builtInStringMethoids

---
+++ builtInStringMethods.java
@@ -0,0 +1,9 @@
+public class builtInStringMethods {
+public static void main(String[] args) {
+    String s= "Yashwant Singh"; //space is also count as Index.
+    System.out.println(s.charAt(0));
+   // System.out.println(s.charAt(20));  //error
+    int n =s.length();
+    System.out.println(n);
+}
+}
