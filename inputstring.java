6845d599ad87f89b5e92e2cb78c99c44c27ad162
Yashwant1304 <yksinghemail@gmail.com>
2025-04-18 16:11:53 +0530

    InputString

---
+++ inputstring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class inputstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name :");
+      //String str = sc.next(); //for one String
+        String str = sc.nextLine(); 
+        System.out.print("Hi ");
+        System.out.println(str);
+
+       
+    }
+}
