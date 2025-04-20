3c0a85b1e603268c3f87bd7173a3182c2c9acded
Yashwant1304 <yksinghemail@gmail.com>
2025-04-20 17:25:02 +0530

    countIndex

---
+++ indexNumber.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+public class indexNumber {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String s = sc.nextLine();
+    for (int i = 0; i < s.length(); i++) {
+        System.out.println("Character at index " + i + ": " + s.charAt(i));
+    }
+        }
+    }
+
