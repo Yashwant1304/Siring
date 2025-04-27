defe63fa03014f50aef3007b5c290128326d647c
Yashwant1304 <yksinghemail@gmail.com>
2025-04-27 22:14:57 +0530

    compareString

---
+++ compareTwoString.java
@@ -0,0 +1,10 @@
+
+
+public class compareTwoString {
+    public static void main(String[] args) {
+        // String s=sc.nextLine();
+        String a="abc";
+        String b="abceee";
+        System.out.println(a.compareTo(b));
+    }
+}
