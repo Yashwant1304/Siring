9796a64d9acb2cd91ad18000379fecaf84792f57
Yashwant1304 <yksinghemail@gmail.com>
2025-04-29 23:40:06 +0530

    contain&Startwith

---
+++ contin.java
@@ -0,0 +1,7 @@
+public class contin {
+    public static void main(String[] args) {
+        String s="Hello i am Yashwant.";
+        // System.out.println(s.contains("ash"));
+        System.out.println(s.startsWith("Hello"));
+    }
+}
